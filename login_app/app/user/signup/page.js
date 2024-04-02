cc28df736010a5415815fa36a66ed9eafccf32a2
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-04-03 00:38:38 +0530

    added middleware

--- login_app/app/user/signup/page.js
+++ login_app/app/user/signup/page.js
@@ -87,7 +87,7 @@ export default function SignupPage() {
             <button
             onClick={onSignup}
             className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
-            <Link href="/login">Visit login page</Link>
+            <Link href="/user/login">Visit login page</Link>
         </div>
     )
 
---
+++ login_app/middleware.js
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+    const path = request.nextUrl.pathname
+    const isPublicPath =path==="/"|| path === "/user/login" || path ==="/user/signup" || path === "/user/verifyemail"
+    const token = request.cookies.get("token")?.value || ""
+
+    if(isPublicPath && token){
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+    if(!isPublicPath && !token){
+        return NextResponse.redirect(new URL('/user/login', request.url))
+    }
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher:['/','/user/login','/user/signup','/user/profile','/user/verifyemail'],
+}
