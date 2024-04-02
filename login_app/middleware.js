9ca9d9b124022a9d4f3ff65972a07cb8464d54a2
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-04-03 01:08:03 +0530

    fixed middleware

--- login_app/middleware.js
+++ login_app/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
     const path = request.nextUrl.pathname
-    const isPublicPath =path==="/"|| path === "/user/login" || path ==="/user/signup" || path === "/user/verifyemail"
+    const isPublicPath = path === "/user/login" || path ==="/user/signup" || path === "/user/verifyemail"
     const token = request.cookies.get("token")?.value || ""
 
     if(isPublicPath && token){
@@ -16,5 +16,5 @@ export function middleware(request) {
  
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher:['/','/user/login','/user/signup','/user/profile','/user/verifyemail'],
+  matcher:['/user/login','/user/signup','/user/profile','/user/verifyemail'],
 }
