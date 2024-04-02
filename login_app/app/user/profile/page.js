d0f35b07e8ddcfe061e165cd24a6ad754cbb4a61
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-04-02 22:09:55 +0530

    completed basic frontend

---
+++ login_app/app/user/profile/page.js
@@ -0,0 +1,54 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React, {useState} from "react";
+import {toast,Toaster} from "react-hot-toast";
+import {useRouter} from "next/navigation";
+
+
+export default function ProfilePage() {
+    const router = useRouter()
+    const [data, setData] = useState("nothing")
+    const logout = async () => {
+        try {
+            await axios.get('/api/user/logout')
+            toast.success('Logout successful')
+            router.push('/user/login')
+        } catch (error) {
+            console.log(error.message);
+            toast.error(error.message)
+        }
+    }
+
+    const getUserDetails = async () => {
+        try {
+            const res = await axios.post('/api/user/me')
+            console.log(res.data);
+            setData(res.data.user._id)
+        } catch (error) {
+            console.log("Details fetch failed", error.message);
+            toast.error(error.response.data.message);
+        }
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <Toaster/>
+            <h1>Profile</h1>
+            <hr />
+            <p>Profile page</p>
+            <h2 className="p-1 rounded bg-green-500">{data === 'nothing' ? "Nothing" : <Link href={`/user/profile/${data}`}>{data}
+            </Link>}</h2>
+        <hr />
+        <button
+        onClick={logout}
+        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >Logout</button>
+
+        <button
+        onClick={getUserDetails}
+        className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >GetUser Details</button>
+            </div>
+    )
+}
--- login_app/app/user/signup/page.js
+++ login_app/app/user/signup/page.js
@@ -1,11 +1,94 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import {useRouter} from "next/navigation";
+import axios from "axios";
+import { toast,Toaster } from "react-hot-toast";
 
-const page = () => {
-  return (
-    <div>
-      Sign up
-    </div>
-  )
+
+
+
+export default function SignupPage() {
+  const awaitDelay = ()=>{
+    return new Promise((resolve)=>{
+      setTimeout(() => {
+        resolve()
+        return
+      }, 1000);
+    })
+  }
+    const router = useRouter();
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: "",
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(true);
+    const [loading, setLoading] = React.useState(false);
+
+    const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/user/signup", user);
+            console.log("Signup success", response.data);
+            toast.success("Signed up successfully, please login")
+            await awaitDelay()
+            router.push("/user/login");
+        } catch (error) {
+            console.log("Signup failed", error.message);
+            
+            toast.error(error.response.data.message);
+        }finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
+
+    return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Toaster/>
+        <h1>{loading ? "Processing" : "Signup"}</h1>
+        <hr />
+        <label htmlFor="username">username</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="username"
+            type="text"
+            value={user.username}
+            onChange={(e) => setUser({...user, username: e.target.value})}
+            placeholder="username"
+            />
+        <label htmlFor="email">email</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="email"
+            type="text"
+            value={user.email}
+            onChange={(e) => setUser({...user, email: e.target.value})}
+            placeholder="email"
+            />
+        <label htmlFor="password">password</label>
+        <input 
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+            id="password"
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({...user, password: e.target.value})}
+            placeholder="password"
+            />
+            <button
+            onClick={onSignup}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
+            <Link href="/login">Visit login page</Link>
+        </div>
+    )
+
 }
-
-export default page
