2f3171fb2b008331f14cafee8cc91e3352a12810
Vignesh D <75655439+Vignesh9123@users.noreply.github.com>
2024-04-04 00:33:15 +0530

    more customization

--- login_app/app/page.js
+++ login_app/app/page.js
@@ -3,16 +3,20 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 export default function Home() {
   const [loggedIn, setLoggedIn] = useState(false)
+  const [loaded, setLoaded] = useState(false)
   useEffect(()=>{
     (async()=>{
+      setLoaded(false)
       const res = await fetch("/api/user/getIsLoggedIn")
       const resp = await res.json()
       setLoggedIn(resp.login)
+      setLoaded(true)
     })()
   },[])
   return (<>
    <div>Home</div>
-   {loggedIn ? <Link href="/user/profile">Sign out</Link>:<Link href="/user/login">Sign in</Link>}
+
+   {loaded &&loggedIn ? <Link href="/user/profile">Sign out</Link>:<Link href="/user/login">Sign in</Link>}
    </>
   );
 }
